48d4a4d2d7e4a155bfb8ead77ffc324ff048cded
uodev <89963503+uodev@users.noreply.github.com>
2021-10-21 00:14:24 +0300

    Create projestart.dart

---
+++ projestart.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Projem',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+        ),
+        home: Scaffold(
+          appBar: AppBar(),
+          body: Center(
+            child: Text("Merhaba"),
+          ),
+        ));
+  }
+}
